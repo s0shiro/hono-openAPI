11906e2417b675492fc9d7e341d9bd9e5b453e62
Nielven Mascariñas <mascarinas022@gmail.com>
2024-10-07 09:56:59 +0800

    define schema for the db

---
+++ src/db/schema.ts
@@ -0,0 +1,13 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text('name').notNull(),
+  done: integer('done', { mode: 'boolean' }).notNull().default(false),
+  createdAt: integer('created_at', { mode: 'timestamp' })
+    .$defaultFn(() => new Date()),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .$defaultFn(() => new Date())
+    .$onUpdate(() => new Date()),
+});
