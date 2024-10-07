201b93ff166da05637504c0d8dde73017d0e9cd8
Nielven Mascariñas <mascarinas022@gmail.com>
2024-10-07 09:40:14 +0800

    set up drizzle orm and added it into env types

--- src/env.ts
+++ src/env.ts
@@ -10,6 +10,19 @@ const EnvSchema = z.object({
   NODE_ENV: z.string().default('develpment'),
   PORT: z.coerce.number().default(9999),
   LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+  DATABASE_URL: z.string().url(),
+  DATABASE_AUTH_TOKEN: z.string().optional(),
+}).superRefine((input, ctx) => {
+  // in production DATABASE_AUTH_TOKEN can't be optional
+  if (input.NODE_ENV === 'production' && !input.DATABASE_AUTH_TOKEN) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.invalid_type,
+      expected: 'string',
+      received: 'undefined',
+      path: ['DATABASE_AUTH_TOKEN'],
+      message: 'Must be set when NODE_ENV is \'production\'',
+    });
+  }
 });
 
 export type env = z.infer<typeof EnvSchema>;
